1a000520907017335800330b2bbf15ccede4b328
geonyeolpark <msmn1729@gmail.com>
2021-04-12 16:12:37 +0900

    file setting

---
+++ src/main/java/com/teamtalk/TeamTalkApplication.java
@@ -0,0 +1,13 @@
+package com.teamtalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamTalkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamTalkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/teamtalk/TeamTalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamtalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamTalkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
